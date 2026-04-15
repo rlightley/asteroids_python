a6664f11e83ec478dce0b4f6f9ceb54897fa50eb
ryanlightley <rlightley88@gmail.com>
2026-04-15 20:01:39 +0100

    Init

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+from logger import log_state
+
+
+def main():
+    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        
+
+if __name__ == "__main__":
+    main()
